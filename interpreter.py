434be32f9423d8ea70e0e337344475288390a1d2
watertim <45728713+watertim@users.noreply.github.com>
2020-07-06 18:42:05 +0800

    Add some comments
    TODO: Detect error when running brainfuck code.

--- interpreter.py
+++ interpreter.py
@@ -1,9 +1,10 @@
+#TODO: Detect Error
 from typing import List, Any
-
 
 def move(listofobj: List[Any]):
     if not isinstance(listofobj, List):
         raise TypeError
+    #Extract input list
     Ended = listofobj[0]
     brainfuckl = listofobj[1]
     reader = listofobj[2]
@@ -11,41 +12,41 @@ def move(listofobj: List[Any]):
     pointer = listofobj[4]
     result = listofobj[5]
 
-    instructor = brainfuckl[reader]
-    if (instructor == '>'):
+    execution = brainfuckl[reader]
+    if (execution == '>'):
         if (len(tap) == pointer + 1):
             tap = tap + [0]
         pointer = pointer + 1
-    elif (instructor == '<'):
+    elif (execution == '<'):
         pointer = pointer - 1
-    elif (instructor == '+'):
+    elif (execution == '+'):
         if (tap[pointer] == 255):
             tap[pointer] = 0
         else:
             tap[pointer] = tap[pointer] + 1
-    elif (instructor == '-'):
+    elif (execution == '-'):
         if (tap[pointer] == 0):
             tap[pointer] = 255
         else:
             tap[pointer] = tap[pointer] - 1
-    elif (instructor == '.'):
+    elif (execution == '.'):
         result = result + chr(tap[pointer])
-    elif (instructor == '['):
+    elif (execution == '['):
         if (tap[pointer] == 0):
-            while (instructor != ']'):
+            while (execution != ']'):
                 if (reader == len(brainfuckl) - 1):
                     return [True, brainfuckl, reader, tap, pointer, result]
                 reader = reader + 1
-                instructor = brainfuckl[reader]
-    elif (instructor == ']'):
+                execution = brainfuckl[reader]
+    elif (execution == ']'):
         if (tap[pointer] != 0):
-            while (instructor != '['):
+            while (execution != '['):
                 if (reader == 0):
                     result = "Pointer went over beginning of the tap!(Mismatched parentheses)\n"
                     return [True, brainfuckl, reader, tap, pointer, result]
                 reader = reader - 1
-                instructor = brainfuckl[reader]
-    elif (instructor == ','):
+                execution = brainfuckl[reader]
+    elif (execution == ','):
         result = "TODO: Implement the input.\n"
         return [True, brainfuckl, reader, tap, pointer, result]
     else:
@@ -63,11 +64,11 @@ def interpreter(brainfuckstr: str) -> None:
     if not isinstance(brainfuckstr, str):
         raise TypeError
     Ended = False
-    brainfuckl = [i for i in brainfuckstr]
-    reader = 0
-    pointer = 0
-    tap = [0]
-    result = ""
+    brainfuckl = [i for i in brainfuckstr] #brainfuck code list(a char/a element)
+    reader = 0 #pointer for brainfuckl
+    tap = [0] #data tap
+    pointer = 0 #pointer for tap
+    result = "" #output
     while True:
         [Ended, brainfuckl, reader, tap, pointer, result] = move([Ended, brainfuckl, reader, tap, pointer, result])
         if (Ended):
