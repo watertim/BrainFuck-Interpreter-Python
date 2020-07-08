4c368f03aab8766e69374f910044ae7ec9cca82e
watertim <45728713+watertim@users.noreply.github.com>
2020-07-09 04:37:54 +0800

    Fix some bugs / Add some "Features"

--- interpreter.py
+++ interpreter.py
@@ -1,80 +1,107 @@
-#TODO: Detect Error
 from typing import List, Any
 
 def move(listofobj: List[Any]):
     if not isinstance(listofobj, List):
         raise TypeError
+    if (len(listofobj) != 7):
+        raise Exception("List Error")
     #Extract input list
-    Ended = listofobj[0]
-    brainfuckl = listofobj[1]
-    reader = listofobj[2]
-    tap = listofobj[3]
-    pointer = listofobj[4]
-    result = listofobj[5]
+    #ERROR = listofobj[0]
+    #Ended = listofobj[1]
+    brainfuckl = listofobj[2]
+    reader = listofobj[3]
+    tap = listofobj[4]
+    pointer = listofobj[5]
+    result = listofobj[6]
 
+    bypass_list=[' ','\n','\r']
     execution = brainfuckl[reader]
-    if (execution == '>'):
+    if (execution == '['):
+        if (tap[pointer] == 0):
+            bypass = 0
+            while (execution != ']' or bypass != -1):
+                reader += 1
+                execution = brainfuckl[reader]
+                if (execution == '['):
+                    bypass += 1
+                elif (execution == ']'):
+                    bypass -= 1
+                else: pass
+
+                if (reader == len(brainfuckl) - 1):
+                        result = "<SyntaxError> Found mismatched square brackets!\n"
+                        return [True, True, brainfuckl, reader, tap, pointer, result]
+    elif(execution == ']'):
+        if (tap[pointer] != 0):
+            bypass = 0
+            while (execution != '[' or bypass != -1):
+                reader -= 1
+                execution = brainfuckl[reader]
+                if (execution == ']'):
+                    bypass += 1
+                elif (execution == '['):
+                    bypass -= 1
+                else: pass
+
+                if (reader == 0):
+                        result = "<SyntaxError> Found mismatched square brackets!\n"
+                        return [True, True, brainfuckl, reader, tap, pointer, result]
+    elif (execution == '>'):
         if (len(tap) == pointer + 1):
-            tap = tap + [0]
-        pointer = pointer + 1
+            tap += [0]
+        pointer += 1
     elif (execution == '<'):
-        pointer = pointer - 1
+        pointer -= 1
     elif (execution == '+'):
         if (tap[pointer] == 255):
             tap[pointer] = 0
         else:
-            tap[pointer] = tap[pointer] + 1
+            tap[pointer] += 1
     elif (execution == '-'):
         if (tap[pointer] == 0):
             tap[pointer] = 255
         else:
-            tap[pointer] = tap[pointer] - 1
+            tap[pointer] -= 1
     elif (execution == '.'):
-        result = result + chr(tap[pointer])
-    elif (execution == '['):
-        if (tap[pointer] == 0):
-            while (execution != ']'):
-                if (reader == len(brainfuckl) - 1):
-                    return [True, brainfuckl, reader, tap, pointer, result]
-                reader = reader + 1
-                execution = brainfuckl[reader]
-    elif (execution == ']'):
-        if (tap[pointer] != 0):
-            while (execution != '['):
-                if (reader == 0):
-                    result = "Pointer went over beginning of the tap!(Mismatched parentheses)\n"
-                    return [True, brainfuckl, reader, tap, pointer, result]
-                reader = reader - 1
-                execution = brainfuckl[reader]
+        result += chr(tap[pointer])
     elif (execution == ','):
-        result = "TODO: Implement the input.\n"
-        return [True, brainfuckl, reader, tap, pointer, result]
+        result = "<NotImplementedError> Not implement the input yet!\n"
+        return [True, True, brainfuckl, reader, tap, pointer, result]
+    elif (execution in bypass_list):#Just bypass
+        result = result
     else:
-        result = "Not a vaild brainfuck code!\n"
-        return [True, brainfuckl, reader, tap, pointer, result]
+        result = "<SyntaxError> Not a vaild brainfuck code!\n"
+        return [True, True, brainfuckl, reader, tap, pointer, result]
 
     if (reader == len(brainfuckl) - 1):
-        return [True, brainfuckl, reader, tap, pointer, result]
+        return [False, True, brainfuckl, reader, tap, pointer, result]
     else:
-        reader = reader + 1
-        return [False, brainfuckl, reader, tap, pointer, result]
+        reader += 1
+        return [False, False, brainfuckl, reader, tap, pointer, result]
 
 
-def interpreter(brainfuckstr: str) -> None:
+def interpreter(brainfuckstr: str) -> str:
     if not isinstance(brainfuckstr, str):
         raise TypeError
-    Ended = False
+    #print(brainfuckstr)
+    ERROR = False
+    END = False
     brainfuckl = [i for i in brainfuckstr] #brainfuck code list(a char/a element)
     reader = 0 #pointer for brainfuckl
     tap = [0] #data tap
     pointer = 0 #pointer for tap
     result = "" #output
     while True:
-        [Ended, brainfuckl, reader, tap, pointer, result] = move([Ended, brainfuckl, reader, tap, pointer, result])
-        if (Ended):
+        [ERROR, END, brainfuckl, reader, tap, pointer, result] = move([ERROR, END, brainfuckl, reader, tap, pointer, result])
+        if (END):
             break
-    print(result, end="")
+    return [ERROR, result]
+
+if __name__ == '__main__':
+    inp = input()
+    print(interpreter(inp))
 
 
 #interpreter("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.,")
 #interpreter("+]+++++++++++++++++++++.+++++++++++++++++++++++++++++++++++++++++++++++++++.")
+#interpreter(">>----[---->+<]>++.++++++++.++++++++++.>-[----->+<]>.+[--->++<]>+++.>-[--->+<]>-.[---->+++++<]>-.[-->+<]>---.[--->++<]>---.++[->+++<]>.+[-->+<]>+.[--->++<]>---.++[->+++<]>.+++.[--->+<]>----.[-->+<]>-----.[->++<]>+.-[---->+++<]>.--------.>-[--->+<]>.-[----->+<]>-.++++++++.--[----->+++<]>.+++.[--->+<]>-.-[-->+<]>---.++[--->+++++<]>.++++++++++++++.+++[->+++++<]>.[----->+<]>++.>-[----->+<]>.---[->++<]>-.++++++.[--->+<]>+++.+++.[-]")
---
+++ main.py
@@ -0,0 +1,73 @@
+from flask import Flask, request
+from flask import render_template, Response
+
+from interpreter import interpreter
+
+from urllib.parse import quote, unquote
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route("/inter", methods=['GET'])
+def inter():
+    try:
+        bf = request.args.get('bf')
+
+        encodeBf = request.args.get('encodeBf')
+
+        print("encodeBf")
+    except:
+        bf = null
+        
+        encodeBf = request.args.get('encodeBf')
+
+        print("bf")
+
+    if bf:
+        return render_template("interprete.html")
+    elif encodeBf:
+        print("in")
+        bf = str(encodeBf)
+        print("---------\n" + bf + "---------\n")
+        bf = unquote(bf)
+        bf = bf.replace(" ", "")
+        bf = bf.replace("\n", "")
+        bf = bf.replace("\r", "")
+
+        return render_template("interprete.html", err=interpreter(bf)[0], inp=interpreter(bf)[1])
+    else:
+        return render_template("inter.html")
+
+@app.route("/test", methods=['GET'])
+def test(bf):
+    bf = request.args.get('bf')
+    return render_template("interprete.html", err=interpreter(bf)[0], inp=interpreter(bf)[1])
+
+@app.route("/api", methods=['GET'])
+def api():
+    bf = request.query_string.decode()
+    bf = bf.replace("bf=","")
+    if bf:
+        print("1.########" + bf + "########\n")
+        bf = bf.replace("%20", "")
+        bf = bf.replace("\n", "")
+        bf = bf.replace("\r", "")
+        bf = bf.replace(" ", "+")
+        bf = unquote(bf)
+        print("2.########" + bf + "########\n")
+
+        result = interpreter(bf)
+        
+        if (result[0]):
+            return Response(f"Error Found:\n{interpreter(bf)[1]}", mimetype='text/plain')
+        else:
+            return Response(interpreter(bf)[1], mimetype='text/plain')
+    else:
+
+        return Response("Error Found:\n<SyntaxError> No Brainfuck code provided!", mimetype='text/plain')
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host="10.0.2.15",port="5000")
